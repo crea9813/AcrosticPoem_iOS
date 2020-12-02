7836488a8379933f0d349283a842ec52181b2d58
Potos <crea9813@gmail.com>
2020-12-02 23:17:02 +0900

    Initial Commit

---
+++ AcrosticPoemAdmin/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AcrosticPoemAdmin
+//
+//  Created by Poto on 2020/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
